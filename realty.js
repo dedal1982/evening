a170b8187793889a8a3a8f41bbdf1aa9eb9376d0
aldm <d44.a1@yandex.ru>
2024-09-20 23:31:57 +0300

    1

--- realty.js
+++ realty.js
@@ -1,14 +1,3 @@
-const realtyPriceBtn = document.querySelector(".realty__price");
-const realtyInputs = document.querySelector(".realty__inputs");
-const realtyPriceBracket = document.querySelector(".realty__price-img");
-
-if (realtyPriceBtn) {
-  realtyPriceBtn.addEventListener("click", () => {
-    realtyInputs.classList.toggle("active");
-    realtyPriceBracket.classList.toggle("active");
-  });
-}
-
 const swiperRealty = new Swiper(".realty__swiper", {
   // Optional parameters
   direction: "horizontal",
